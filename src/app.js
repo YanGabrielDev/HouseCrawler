866e1c92e96c6c7c7de8c5cf179ea7dab3af0858
Yan_Gabriel <yangabriel2012@gmail.com>
2024-10-30 19:53:47 -0400

    configurando deploy

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ import { housesCrawler } from "./services/quinto-andar-service.js";
 async function routes(fastify, options) {
     fastify.get('/', async (request, reply) => {
         await housesCrawler();
-        reply.code(200).send({ message: "Busca realizada com sucesso" })
-    })
+        reply.code(200).send({ message: "Busca realizada com sucesso" });
+    });
 }
 
 export default routes;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,18 @@
 "use strict";
 
-import Fastify from 'fastify'
+import Fastify from 'fastify';
 import * as dotenv from "dotenv";
 dotenv.config();
+
 const app = Fastify({
     logger: true
-})
+});
 
-// fastify.get('/', async function (request, reply) {
-//     await housesCrawler();
-//     reply.code(200).send({ message: "Busca realizada com sucesso" })
-// })
-
-// Register your application as a normal plugin.
+// Registrar as rotas do app
 app.register(import("./app.js"));
 
+// Função de exportação para a Vercel
 export default async (req, res) => {
     await app.ready();
     app.server.emit('request', req, res);
-}
-
-// try {
-//     await fastify.listen({ port: 3000 })
-// } catch (err) {
-//     fastify.log.error(err)
-//     process.exit(1)
-// }
+};
