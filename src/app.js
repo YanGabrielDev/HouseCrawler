ada631a25a82060a6dc601d57779155193445abe
Yan_Gabriel <yangabriel2012@gmail.com>
2024-10-30 19:46:22 -0400

    configurando deploy

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import { housesCrawler } from "./services/quinto-andar-service.js";
+
+async function routes(fastify, options) {
+    fastify.get('/', async (request, reply) => {
+        await housesCrawler();
+        reply.code(200).send({ message: "Busca realizada com sucesso" })
+    })
+}
+
+export default routes;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
-import { housesCrawler } from "./services/quinto-andar-service.js";
-import Fastify from 'fastify'
+"use strict";
 
-const fastify = Fastify({
+import Fastify from 'fastify'
+import * as dotenv from "dotenv";
+dotenv.config();
+const app = Fastify({
     logger: true
 })
 
-fastify.get('/', async function (request, reply) {
-    await housesCrawler();
-    reply.code(200).send({ message: "Busca realizada com sucesso" })
-})
+// fastify.get('/', async function (request, reply) {
+//     await housesCrawler();
+//     reply.code(200).send({ message: "Busca realizada com sucesso" })
+// })
 
+// Register your application as a normal plugin.
+app.register(import("./app.js"));
 
-
-try {
-    await fastify.listen({ port: 3000 })
-} catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
+export default async (req, res) => {
+    await app.ready();
+    app.server.emit('request', req, res);
 }
+
+// try {
+//     await fastify.listen({ port: 3000 })
+// } catch (err) {
+//     fastify.log.error(err)
+//     process.exit(1)
+// }
