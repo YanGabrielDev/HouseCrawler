938a4614d00316492d74c2a8fc0a8e57ead7384a
Yan_Gabriel <yangabriel2012@gmail.com>
2024-10-30 19:21:10 -0400

    criando rota para chamar o crawler

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import { housesCrawler } from "./services/quinto-andar-service.js";
+import Fastify from 'fastify'
+
+const fastify = Fastify({
+    logger: true
+})
+
+fastify.get('/', async function (request, reply) {
+    await housesCrawler();
+    reply.code(200).send({ message: "Busca realizada com sucesso" })
+})
 
 
 
-housesCrawler();
+try {
+    await fastify.listen({ port: 3000 })
+} catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+}
