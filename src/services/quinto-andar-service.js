760733ae04c11ebed11e901c64db0f0c018088f6
Yan_Gabriel <yangabriel2012@gmail.com>
2024-10-30 19:22:26 -0400

    criando o service de envio de email

--- src/services/quinto-andar-service.js
+++ src/services/quinto-andar-service.js
@@ -1,7 +1,8 @@
 import { listHouses } from "../api/quinto-andar-api.js"
 import { promises as fs } from 'fs';
 import { getFileData } from "../utils/getFileData.js";
-
+import { sendEmail } from "./send-email-service.js";
+import { newHouse } from '../mock/housesMock.js'
 /**
  * função principal do crawler que faz a busca dos apartamentos
  * @returns 
@@ -14,7 +15,7 @@ export const housesCrawler = async () => {
         const housesJson = JSON.stringify(houses);
 
         if (savedHouses) {
-            await handlCheckSavedHouses({ houses, housesJson, parsedSavedHouses })
+            await handlCheckSavedHouses({ houses: [...houses, newHouse], housesJson, parsedSavedHouses })
             return
         }
 
@@ -39,7 +40,8 @@ export const housesCrawler = async () => {
 const handlCheckSavedHouses = async ({ houses = [], housesJson = '', parsedSavedHouses = [] }) => {
     const newHouses = houses.filter(house => !parsedSavedHouses.some(savedHouses => savedHouses._id === house._id)) || []
     if (newHouses.length > 0) {
-        console.log("Novos apartamentos encontrados", newHouses);
+        const houseslinks = newHouses.map(houses => `https://www.quintoandar.com.br/imovel/${houses?._id}/comprar`)
+        await sendEmail(houseslinks)
         await fs.unlink('houses.json')
         await fs.writeFile('houses.json', housesJson);
 
