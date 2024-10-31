96ffa447014aba304cf174cc5d26599c935f4bde
Yan_Gabriel <yangabriel2012@gmail.com>
2024-10-30 23:27:17 -0400

    teste

--- src/api/quinto-andar-api.js
+++ src/api/quinto-andar-api.js
@@ -1,27 +1,65 @@
-import { QUINTO_ANDAR_API } from "../config/apis.js";
-import { postBody } from "../config/post-body.js";
+import { listHouses } from "../api/quinto-andar-api.js";
+import { promises as fs } from 'fs';
+import { getFileData } from "../utils/getFileData.js";
+import { sendEmail } from "./send-email-service.js";
+import { newHouse } from '../mock/housesMock.js';
+import path from 'path';
 
-export const listHouses = async () => {
+// Define o caminho absoluto para o arquivo JSON
+const filePath = path.resolve(__dirname, 'houses.json');
+
+/**
+ * Função principal do crawler que faz a busca dos apartamentos
+ * @returns
+ */
+export const housesCrawler = async () => {
     try {
-        console.log("Iniciando a busca...");
+        const houses = await listHouses();
 
-        // Realiza a requisição POST
-        const response = await fetch(QUINTO_ANDAR_API, {
-            method: "POST",
-            body: JSON.stringify(postBody),
-            headers: { 'Content-Type': 'application/json' } // Adiciona cabeçalhos se necessário
-        });
+        // Verifica se o arquivo já existe e lê seu conteúdo
+        let savedHouses = '';
+        try {
+            savedHouses = await getFileData(filePath);
+        } catch (error) {
+            console.log("Arquivo 'houses.json' não encontrado. Um novo será criado.");
+        }
 
-        // Transforma a resposta em JSON
-        const data = await response.json();
+        const parsedSavedHouses = savedHouses ? JSON.parse(savedHouses) : [];
+        const housesJson = JSON.stringify(houses);
 
-        // Extração de dados relevantes
-        const houses = data.hits.hits.map(item => item);
+        if (savedHouses) {
+            await handlCheckSavedHouses({ houses: [...houses, newHouse], housesJson, parsedSavedHouses });
+            return { new: [...houses, newHouse], save: parsedSavedHouses };
+        }
 
+        if (housesJson) {
+            await fs.writeFile(filePath, housesJson);
+            console.log(`${houses.length} apartamentos encontrados`);
+            console.log("Arquivo 'houses.json' salvo com sucesso.");
+        }
 
-        return houses
-
+        console.log("Busca realizada com sucesso");
     } catch (error) {
         console.error("Erro ao concluir busca:", error);
+    }
+};
+
+/**
+ * Verifica se algum apartamento novo foi encontrado na busca
+ * @param {Array} houses Array de apartamentos encontrados na api
+ * @param {string} housesJson Json de apartamentos encontrados na api
+ * @param {Array} parsedSavedHouses Array de apartamentos armazenados na ultima busca
+ */
+const handlCheckSavedHouses = async ({ houses = [], housesJson = '', parsedSavedHouses = [] }) => {
+    const newHouses = houses.filter(house => !parsedSavedHouses.some(savedHouse => savedHouse._id === house._id)) || [];
+
+    if (newHouses.length > 0) {
+        const houseLinks = newHouses.map(house => `https://www.quintoandar.com.br/imovel/${house?._id}/comprar`);
+        await sendEmail(houseLinks);
+
+        // Remove o arquivo existente e salva o novo
+        await fs.unlink(filePath).catch(() => console.log("Nenhum arquivo anterior para remover."));
+        await fs.writeFile(filePath, housesJson);
+        console.log("Arquivo 'houses.json' atualizado com novos apartamentos.");
     }
 };
