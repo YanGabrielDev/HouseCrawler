18534a859644c80f3eb0fb126850dc60852c3941
Yan_Gabriel <yangabriel2012@gmail.com>
2024-10-30 20:34:12 -0400

    teste

--- src/app.js
+++ src/app.js
@@ -1,10 +1,36 @@
-import { housesCrawler } from "./services/quinto-andar-service.js";
+// "use strict";
 
-async function routes(fastify, options) {
-    fastify.get('/', async (request, reply) => {
-        await housesCrawler();
-        reply.code(200).send({ message: "Busca realizada com sucesso" });
+// import Fastify from 'fastify';
+// import * as dotenv from "dotenv";
+// dotenv.config();
+
+// const app = Fastify({
+//     logger: true
+// });
+
+// // Registrar as rotas do app
+// app.register(import("./app.js"));
+
+// // Função de exportação para a Vercel
+// export default async (req, res) => {
+//     await app.ready();
+//     app.server.emit('request', req, res);
+// };
+
+import routes from "./routes";
+
+async function app(
+    instance,
+    opts,
+    done
+) {
+    instance.get("/", async (req, res) => {
+        res.status(200).send({
+            hello: "World",
+        });
     });
+    instance.register(routes, { prefix: "/api/v1" });
+    done();
 }
 
-export default routes;
+export default app;
